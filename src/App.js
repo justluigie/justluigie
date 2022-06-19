8ad3c12ae762f9e3f75a7793ce2ee9f02d82938b
justluigie <luigie.jebulan@yahoo.com>
2022-06-20 04:40:40 +0800

    new revisions

--- src/App.js
+++ src/App.js
@@ -28,17 +28,23 @@ function App() {
             <Particles options={particlesOptions} init={particlesInit}/>
             <div class ="row">
                 <div class="column">
+                    <div id = "horizon">
                     <div id="top">Hi.</div>
                     <div id="middle">i'm </div>
                     <div id="bottom">Luigie.
                     <div id="imp"> A Devops Engineer</div></div>
+                    </div>
                 </div> 
                 <div class="column">
                      <div id="myself"><img id="picture" src={image}/></div>
                 </div>
             </div>
             <div id="short" class ="intro">
-            Under Construction ...
+            I'm a Devops Engineer located in Philippines. I have a passion for IT as a whole but mainly focused on learning cloud based systems.<br></br>
+            <br></br>
+            A dynamic and performance driven graduate of Electronics Engineering, seeking new opportunities to learn. Always up for an insightful conversation over coffee, a fan of anime/video games and always seeking new adventures and places to travel to.<br></br>
+            <br></br>
+            Interested in front-end and back-end technologies. Aiming to eventually have a significant mastery of it.
             </div>
           
             
