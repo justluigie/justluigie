4ce1469d21cd3d9b466cf99c67b5df0e35112598
justluigie <luigie.jebulan@yahoo.com>
2022-06-20 03:55:08 +0800

    2nd rev

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React, { useCallback } from 'react';
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
-
+import { useSpring, animated } from 'react-spring';
+import image from './pics/me.png'
 import './App.css';
 import particlesOptions from "./particles.json";
-import Button from 'react-bootstrap/Button'
+
+
+
+
+
+
+
 
 function App() {
     const particlesInit = useCallback(main => {
         loadFull(main);
     }, [])
 
+
+    
+
+
+
     return (
         <div className="App">
             <Particles options={particlesOptions} init={particlesInit}/>
-            <header className="App-header">
-                Under Construction..
-               
-            </header>
+            <div class ="row">
+                <div class="column">
+                    <div id="top">Hi.</div>
+                    <div id="middle">i'm </div>
+                    <div id="bottom">Luigie.
+                    <div id="imp"> A Devops Engineer</div></div>
+                </div> 
+                <div class="column">
+                     <div id="myself"><img id="picture" src={image}/></div>
+                </div>
+            </div>
+            <div id="short" class ="intro">
+            Under Construction ...
+            </div>
+          
+            
         </div>
+        
     );
 }
 
